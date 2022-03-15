235058159532214a458ca3a33265163a0f4a113a
Dirk Kok <the@dirkkok.nl>
2022-03-15 12:04:41 +0100

    je bolle mams

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus;
+
+var client = new DiscordClient(new DiscordConfiguration() {
+	Token = Environment.GetEnvironmentVariable("BOT_TOKEN")
+});
+client.MessageCreated += (_, args) => {
+	if (!args.Author.IsBot && args.Message.Content.ToLower() == "je bolle mams") {
+		return args.Message.RespondAsync("je bolle mams");
+	} else {
+		return Task.CompletedTask;
+	}
+};
+await client.ConnectAsync();
+await Task.Delay(-1);
